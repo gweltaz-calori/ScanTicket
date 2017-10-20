407c458e610eaa8c32ec0c2a6c11d5d26d85e62a
Gweltaz <gweltaz@Mac-mini-de-Gweltaz.local>
2017-10-20 11:44:26 +0200

    cleaning

--- app/src/main/java/com/example/gweltaz/scanticket/activity/ScanPreviewActivity.java
+++ app/src/main/java/com/example/gweltaz/scanticket/activity/ScanPreviewActivity.java
@@ -4,8 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.example.gweltaz.scanticket.R;
 import com.example.gweltaz.scanticket.utils.Utils;
